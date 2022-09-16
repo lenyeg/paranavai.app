3d21bb10aaf5c15a8546aab02f3e7d7a1440791f
Lucas <lucas.ribeiro711@gmail.com>
2022-09-16 15:08:33 -0300

    style: remove box shadow on customers item

--- src/components/TheCustomers.vue
+++ src/components/TheCustomers.vue
@@ -9,22 +9,22 @@
       class="flex w-full gap-4 p-4 overflow-x-scroll scroll-p-3 snap-mandatory snap-x touch-pan-x lg:snap-none lg:overflow-auto lg:px-0 lg:justify-between xl:max-w-[80%]"
     >
       <div
-        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md shadow-md min-w-[120px] h-[40px]"
+        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md min-w-[120px] h-[40px]"
       ></div>
       <div
-        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md shadow-md min-w-[120px] h-[40px]"
+        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md min-w-[120px] h-[40px]"
       ></div>
       <div
-        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md shadow-md min-w-[120px] h-[40px]"
+        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md min-w-[120px] h-[40px]"
       ></div>
       <div
-        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md shadow-md min-w-[120px] h-[40px]"
+        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md min-w-[120px] h-[40px]"
       ></div>
       <div
-        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md shadow-md min-w-[120px] h-[40px]"
+        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md min-w-[120px] h-[40px]"
       ></div>
       <div
-        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md shadow-md min-w-[120px] h-[40px]"
+        class="flex flex-col justify-center items-center gap-4 snap-start text-center bg-[#FAD1B0] rounded-md min-w-[120px] h-[40px]"
       ></div>
     </div>
   </div>
