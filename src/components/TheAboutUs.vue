e0c90e4a1aa1740040a23f88c12149f0bb614f4c
Lucas <lucas.ribeiro711@gmail.com>
2022-09-16 16:06:55 -0300

    fix: text on second card - about us

--- src/components/TheAboutUs.vue
+++ src/components/TheAboutUs.vue
@@ -42,17 +42,18 @@
       >
         <p class="text-2xl text-gray-700 lg:text-3xl">
           <strong
-            >Se eu procurar no Google um produto ou serviço que você oferece,
-            vou encontrar sua empresa?</strong
+            >As pessoas conhecem o nível de excelência do seu atendimento,
+            produto ou serviço?</strong
           >
         </p>
         <p>
-          Uma pesquisa encomendada pelo Google indicou que 96% dos brasileiros
-          procuram por produtos ou serviços online.
+          Ter boas avaliações no Google é fundamental para que seu negócio ocupe
+          as primeiras posições no buscador.
         </p>
         <p>
-          Esteja bem posicionado no buscador e seja encontrado por pessoas que
-          precisam dos seus produtos/serviços próximo a você.
+          Além disso, pessoas que precisam dos seus produtos/serviços e não
+          conhecem sua empresa, se guiam através das avaliações para fechar
+          negócio.
         </p>
       </article>
       <article class="lg:w-1/3">
