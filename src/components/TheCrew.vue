7b959c3087c6259216374bec468c8850f829a5cc
André Vandal <me@andrevandal.dev>
2022-12-14 19:27:43 -0300

    feat: add crew pictures

--- src/components/TheCrew.vue
+++ src/components/TheCrew.vue
@@ -2,7 +2,7 @@
   <section
     class="container overflow-x-hidden transition-all duration-300 ease-linear"
   >
-    <div class="flex justify-center items-center -space-x-4">
+    <div class="flex items-center justify-center -space-x-4">
       <div
         v-for="(item, index) in lenyegers"
         :key="`worker-${index}`"
@@ -10,30 +10,31 @@
         @click="selectItem(index)"
       >
         <div
-          class="flex justify-center items-center object-scale-down gap-2 min-w-max"
+          class="flex items-center justify-center object-scale-down gap-2 min-w-max"
         >
           <img
             draggable="false"
             :src="item.src"
-            alt=""
+            :alt="item.alt"
             width="85"
             height="85"
             class="w-[50px] h-[50px] lg:w-[85px] lg:h-[85px] rounded-full bg-[#288877]"
+            loading="lazy"
           >
           <Transition
-            enter-active-class="duration-150 ease-in-out origin-left transition-all"
+            enter-active-class="transition-all duration-150 ease-in-out origin-left"
             enter-from-class="scale-x-0 opacity-0"
             enter-to-class="scale-x-100 opacity-100"
-            leave-active-class="duration-300 ease-in-out origin-left transition-all"
+            leave-active-class="transition-all duration-300 ease-in-out origin-left"
             leave-from-class="scale-x-100 opacity-100"
             leave-to-class="scale-x-0 opacity-0"
             appear
           >
             <div
               v-show="item.opened"
-              class="flex flex-col justify-center items-center pr-6 text-gray-800"
+              class="flex flex-col items-center justify-center pr-6 text-gray-800"
             >
-              <span class="whitespace-nowrap font-bold">{{ item.name }}</span>
+              <span class="font-bold whitespace-nowrap">{{ item.name }}</span>
               <span>{{ item.desc }}</span>
             </div>
           </Transition>
@@ -53,29 +54,29 @@ export default defineComponent({
   setup () {
     let lenyegers = reactive([
       {
-        src: '/images/ceo.png',
-        alt: 'Core team',
+        src: '/images/crew/andre.vandal-80_x_80.png',
+        alt: 'André Vandal',
         name: 'André Vandal',
         desc: 'CEO',
         opened: false,
       },
       {
-        src: '/images/pm.png',
-        alt: 'Core team',
+        src: '/images/crew/ana.longhini-80_x_80.png',
+        alt: 'Ana Longhini',
         name: 'Ana Longhini',
         desc: 'Product Manager',
         opened: false,
       },
       {
-        src: '/images/gd.png',
-        alt: 'Core team',
+        src: '/images/crew/pedro.henrique-80_x_80.png',
+        alt: 'Pedro Henrique',
         name: 'Pedro Henrique',
         desc: 'UI/UX Graphic',
         opened: false,
       },
       {
-        src: '/images/fed.png',
-        alt: 'Core team',
+        src: '/images/crew/lucas.ribeiro-80_x_80.png',
+        alt: 'Lucas Ribeiro',
         name: 'Lucas Ribeiro',
         desc: 'Front-end Developer',
         opened: false,
